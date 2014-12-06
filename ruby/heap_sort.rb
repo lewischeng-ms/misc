4f8878ac4f1a45341cb9befb1649abfdc642ed53
Lewis Cheng <licheng@microsoft.com>
2014-12-06 21:25:16 +0800

    Fix heapsort in ruby

--- ruby/heap_sort.rb
+++ ruby/heap_sort.rb
@@ -6,10 +6,39 @@ def right_child(i)
   i * 2 + 2
 end
 
-def heap_sort(a)
-  (a.length / 2 - 1).downto(0) do |i|
-    max_heapify(a, i)
+def max_heapify(a, n, i)
+  left = left_child(i)
+  right = right_child(i)
+  if left < n and a[left] > a[i]
+    largest = left
+  else
+    largest = i
   end
+  if right < n and a[right] > a[largest]
+    largest = right
+  end
+  if largest != i
+    t = a[i]
+    a[i] = a[largest]
+    a[largest] = t
+    max_heapify(a, n, largest)
+  end
+end
+
+def heap_sort!(a)
+  n = a.length
+  (n / 2 - 1).downto(0) do |i|
+    max_heapify(a, n, i)
+  end
+
+  (n - 1).downto(1) do |i|
+    t = a[i]
+    a[i] = a[0]
+    a[0] = t
+    max_heapify(a, i, 0)
+  end
+  
+  return a
 end
 
 cases = [
@@ -25,4 +54,4 @@ cases = [
   [ 6, 5, 4, 3, 2, 1 ]
 ]
 
-(0...cases.length).each { |k| printf "Case %d: %s\n", k, heap_sort(cases[k]).join(" ") }
+(0...cases.length).each { |k| printf "Case %d: %s\n", k, heap_sort!(cases[k]).join(" ") }
