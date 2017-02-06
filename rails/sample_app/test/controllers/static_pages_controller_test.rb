53703f9129993fe8a8788d465fb72af6e591f5ba
Lewis Cheng <licheng@microsoft.com>
2017-02-06 22:58:57 +0800

    Update sample_app to rails v5.0.1

--- rails/sample_app/test/controllers/static_pages_controller_test.rb
+++ rails/sample_app/test/controllers/static_pages_controller_test.rb
@@ -1,31 +1,39 @@
 require 'test_helper'
 
-class StaticPagesControllerTest < ActionController::TestCase
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
-  test "should get home" do
-    get :home
+  test "should get root" do
+    get root_url
     assert_response :success
-    assert_select "title", @base_title
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
-    get :help
+    get static_pages_help_url
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
-    get :about
+    get static_pages_about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
-    get :contact
+    get static_pages_contact_url
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
+
 end
