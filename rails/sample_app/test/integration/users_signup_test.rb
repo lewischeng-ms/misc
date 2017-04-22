7a15d684eb3e3055652257c254b3023e829d830f
Lewis Cheng <licheng@microsoft.com>
2017-04-22 21:26:11 +0800

    Add user microposts

--- rails/sample_app/test/integration/users_signup_test.rb
+++ rails/sample_app/test/integration/users_signup_test.rb
@@ -17,7 +17,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.alert-danger'
-    assert_select 'form[action="/signup"]'
   end
 
   test "valid signup information with account activation" do
